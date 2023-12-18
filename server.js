88309cc81ece6c116e135f6148465031b33fc8c2
Stan1-0 <stankofb@gmail.com>
2023-12-18 05:12:06 +0000

    product module

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 //routes
@@ -6,7 +8,20 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello Node API");
 });
-
-app.listen(3000, () => {
-  console.log("Node API  app is running on port 300");
+app.get("/blog", (req, res) => {
+  res.send("Hello blog,My name is Stanley");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://stankofb:StAvenue@cluster0.cef6sjr.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Mongodb connected successfully!");
+    app.listen(3000, () => {
+      console.log("Node API  app is running on port 300");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
